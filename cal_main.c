3f8deac8b2a56201190edefd337b115eb0c283b1
jinselee <jinsezzang11@gmail.com>
2025-08-27 13:57:47 +0900

    Update cal_main.c

--- cal_main.c
+++ cal_main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <arithmetic.h>
+#include "arithmetic.h" 
 
-int main(int argc, char *argv[])
-{
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
+int main(int argc, char* argv[]) {
+    int a = 0, b = 0;
+    if (argc >= 3) { a = atoi(argv[1]); b = atoi(argv[2]); }
 
     printf("%d + %d => %d\n", a, b, add(a, b));
     printf("%d - %d => %d\n", a, b, sub(a, b));
     printf("%d * %d => %d\n", a, b, mul(a, b));
-    printf("%d / %d => %d\n", a, b, dur(a, b));
-
+    printf("%d / %d => %d\n", a, b, dur(a, b)); 
     return 0;
 }
+
