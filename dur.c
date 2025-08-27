573ec8cf54519e0e6970e17ccf6849ad1fe05a7e
jinselee <jinsezzang11@gmail.com>
2025-08-27 14:17:39 +0900

    Update dur.c

--- dur.c
+++ dur.c
@@ -1,6 +1,21 @@
-#include <arithmetic.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "arithmetic.h"
 
-int dur(int a, int b) {
-    if (b == 0) return 0;
-    return a / b;
+int main(int argc, char* argv[]) {
+    int a = 0, b = 0;
+    if (argc >= 3) { a = atoi(argv[1]); b = atoi(argv[2]); }
+
+    printf("%d + %d => %d\n", a, b, add(a, b));
+    printf("%d - %d => %d\n", a, b, sub(a, b));
+    printf("%d * %d => %d\n", a, b, mul(a, b));
+
+    if (b == 0) {
+ 
+        printf("%d / %d => 계산할 수 없습니다.\n", a, b);
+
+    } else {
+        printf("%d / %d => %d\n", a, b, dur(a, b));
+    }
+    return 0;
 }
